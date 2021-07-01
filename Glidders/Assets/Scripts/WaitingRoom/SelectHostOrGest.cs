ed1370bf50308e85e0d67213a9843f5ae6749cdd
nkc-TakaoOnda <tnk5zdmr7uegh4i2i13m@icloud.com>
2021-07-01 19:54:58 +0900

    ホストとゲストが繋がりました

--- Glidders/Assets/Scripts/WaitingRoom/SelectHostOrGest.cs
+++ Glidders/Assets/Scripts/WaitingRoom/SelectHostOrGest.cs
@@ -3,21 +3,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
 
 namespace Glidders
 {
-    public class SelectHostOrGest : MonoBehaviour
+    public class SelectHostOrGest : MonoBehaviourPunCallbacks
     {
+        [SerializeField] GameObject createButton;
+        [SerializeField] GameObject lookForButton;
+        [SerializeField] GameObject createRoom;
+        [SerializeField] GameObject lookForRoom;
+        [SerializeField] GameObject canCreate1;
+        [SerializeField] GameObject canCreate2;
+        [SerializeField] GameObject returnButton;
+
+
+        [SerializeField] GameObject createRoomText;
+        [SerializeField] GameObject lookForRoomText;
+
         [SerializeField] private CommandInput commandInput;
+
+        private bool isCreate = false;
+        private bool isRoomIn = false;
 
         private delegate void CommandInputFunction();
         private CommandInputFunction[] commandInputFunctionTable;
 
+        private PWaitingRoom photonCS;
         private enum SelectCommand
         {
             COMMAND_NOT_INPUT,
             COMMAND_INPUT_1,
             COMMAND_INPUT_2,
+            COMMAND_INPUT_3,
+            COMMAND_INPUT_4,
+            COMMAND_INPUT_5,
 
             COMMAND_NUMBER
         }
@@ -25,16 +46,31 @@ namespace Glidders
         // Start is called before the first frame update
         void Start()
         {
+            photonCS = GameObject.Find("PhotonManager").GetComponent<PWaitingRoom>();
+
             commandInputFunctionTable = new CommandInputFunction[(int)SelectCommand.COMMAND_NUMBER];
             commandInputFunctionTable[(int)SelectCommand.COMMAND_NOT_INPUT] = CommandNotInput;
             commandInputFunctionTable[(int)SelectCommand.COMMAND_INPUT_1] = CommandInput1;
             commandInputFunctionTable[(int)SelectCommand.COMMAND_INPUT_2] = CommandInput2;
+            commandInputFunctionTable[(int)SelectCommand.COMMAND_INPUT_3] = CommandInput3;
+            commandInputFunctionTable[(int)SelectCommand.COMMAND_INPUT_4] = CommandInput4;
+            commandInputFunctionTable[(int)SelectCommand.COMMAND_INPUT_5] = CommandInput5;
+
+            createButton.SetActive(true);
+            lookForButton.SetActive(true);
+            createRoom.SetActive(false);
+            canCreate1.SetActive(false);
+            canCreate2.SetActive(false);
+            returnButton.SetActive(false);
+
+            createRoomText.SetActive(false);
+            lookForRoomText.SetActive(false);
         }
 
             // Update is called once per frame
         void Update()
         {
-
+            commandInputFunctionTable[commandInput.GetInputNumber()]();
         }
 
         public void SetCommandTab()
@@ -44,18 +80,26 @@ namespace Glidders
 
         public void CommandUpdate()
         {
-            commandInputFunctionTable[commandInput.GetInputNumber()]();
         }
 
         private void CommandNotInput()
         {
             int selectNumber = commandInput.GetSelectNumber();
-            selectNumber = Mathf.Clamp(selectNumber, 0, 2);
+            selectNumber = Mathf.Clamp(selectNumber, 0, 6);
         }
 
         private void CommandInput1()
         {
             commandInput.SetInputNumber(0);
+
+            createButton.SetActive(false);
+            lookForButton.SetActive(false);
+            createRoom.SetActive(true);
+            canCreate1.SetActive(true);
+            canCreate2.SetActive(false);
+            returnButton.SetActive(true);
+
+            createRoomText.SetActive(true);
             
         }
 
@@ -63,6 +107,46 @@ namespace Glidders
         {
             commandInput.SetInputNumber(0);
 
+            createButton.SetActive(false);
+            lookForButton.SetActive(false);
+            createRoom.SetActive(true);
+            canCreate1.SetActive(false);
+            canCreate2.SetActive(true);
+            returnButton.SetActive(true);
+
+            lookForRoomText.SetActive(true);
+        }
+
+        private void CommandInput3()
+        {
+            commandInput.SetInputNumber(0);
+
+            if (isCreate) return;
+            isCreate = true;
+            photonCS.CrateRoom();
+        }
+
+        private void CommandInput4()
+        {
+            commandInput.SetInputNumber(0);
+
+            if (isRoomIn) return;
+            isRoomIn = true;
+            photonCS.JoinRoom();
+        }
+
+        private void CommandInput5()
+        {
+            commandInput.SetInputNumber(0);
+
+            createButton.SetActive(true);
+            lookForButton.SetActive(true);
+            createRoom.SetActive(false);
+            canCreate1.SetActive(false);
+            canCreate2.SetActive(false);
+
+            createRoomText.SetActive(false);
+            lookForRoomText.SetActive(false);
         }
     }
 }
