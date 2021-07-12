88568190603c5c3776ca647dc66f5867233dafa6
nkc-TakaoOnda <tnk5zdmr7uegh4i2i13m@icloud.com>
2021-07-12 13:39:49 +0900

    スタート画面の手直し

--- Glidders/Assets/Scripts/WaitingRoom/Legacy/StartSelect.cs
+++ Glidders/Assets/Scripts/WaitingRoom/Legacy/StartSelect.cs
@@ -11,9 +11,6 @@ namespace Glidders
         [SerializeField] private CommandInput commandInput;
         
         [SerializeField] GameObject startRoomPanel;
-        [SerializeField] GameObject createRoomPanel;
-        [SerializeField] GameObject joinRoomPanel;
-        [SerializeField] GameObject commonPanel;
 
         [SerializeField] Text nameText;
         [SerializeField] Text roomNameText;
@@ -29,10 +26,7 @@ namespace Glidders
             COMMAND_INPUT_1,
             COMMAND_INPUT_2,
             COMMAND_INPUT_3,
-            COMMAND_INPUT_4,
-            COMMAND_INPUT_5,
-            COMMAND_INPUT_6,
-
+          
             COMMAND_NUMBER
         }
 
@@ -44,16 +38,10 @@ namespace Glidders
             commandInputFunctionTable[(int)SelectCommand.COMMAND_INPUT_1] = CommandInput1;
             commandInputFunctionTable[(int)SelectCommand.COMMAND_INPUT_2] = CommandInput2;
             commandInputFunctionTable[(int)SelectCommand.COMMAND_INPUT_3] = CommandInput3;
-            commandInputFunctionTable[(int)SelectCommand.COMMAND_INPUT_4] = CommandInput4;
-            commandInputFunctionTable[(int)SelectCommand.COMMAND_INPUT_5] = CommandInput5;
-            commandInputFunctionTable[(int)SelectCommand.COMMAND_INPUT_6] = CommandInput6;
 
             createjoinSc = GameObject.Find("PhotonManager").GetComponent<PhotonCreateOrJoin>();
 
             startRoomPanel.SetActive(true);
-            createRoomPanel.SetActive(false);
-            joinRoomPanel.SetActive(false);
-            commonPanel.SetActive(false);
         }
 
         // Update is called once per frame
@@ -65,48 +53,15 @@ namespace Glidders
         private void CommandNotInput()
         {
             int selectNumber = commandInput.GetSelectNumber();
-            selectNumber = Mathf.Clamp(selectNumber, 0, 6);
+            selectNumber = Mathf.Clamp(selectNumber, (int)SelectCommand.COMMAND_NOT_INPUT, (int)SelectCommand.COMMAND_INPUT_3);
         }
 
-        private void CommandInput1() //名前決定のボタン
+        private void CommandInput1() //部屋名を入力して決定するボタン
         {
             commandInput.SetInputNumber(0);
 
-            if (nameText.text != "")
-            {
-                PublicStaticBool.isName = true;
-                PhotonNetwork.NickName = nameText.text;
-            }
-            else
-            {
-                PublicStaticBool.isName = false;
-            }
-        }
-
-        private void CommandInput2() //部屋を作る画面に移動するボタン
-        {
-            commandInput.SetInputNumber(0);
-
-            if (!(PublicStaticBool.isName)) return;
-            createRoomPanel.SetActive(true);
-            commonPanel.SetActive(true);
-            startRoomPanel.SetActive(false);
-        }
-
-        private void CommandInput3() //部屋を探す画面に移動するボタン
-        {
-            commandInput.SetInputNumber(0);
-
-            if (!(PublicStaticBool.isName)) return;
-            joinRoomPanel.SetActive(true);
-            commonPanel.SetActive(true);
-            startRoomPanel.SetActive(false);
-        }
-
-        private void CommandInput4() //部屋名を入力して決定するボタン
-        {
-            commandInput.SetInputNumber(0);
-
+            if (!(nameText.text != "")) return;
+            PhotonNetwork.NickName = nameText.text;
             if (roomNameText.text != "")
             {
                 PublicStaticBool.isCreate = true;
@@ -115,13 +70,15 @@ namespace Glidders
             else
             {
                 PublicStaticBool.isCreate = false;
-            }
+            }    
         }
 
-        private void CommandInput5()//部屋名を探して決定するボタン
+        private void CommandInput2()//部屋名を探して決定するボタン
         {
             commandInput.SetInputNumber(0);
 
+            if (!(nameText.text != "")) return;
+            PhotonNetwork.NickName = nameText.text;
             if (roomNameText.text != "")
             {
                 PublicStaticBool.isJoin = true;
@@ -133,14 +90,11 @@ namespace Glidders
             }
         }
 
-        private void CommandInput6() //1つ前に戻るボタン
+        private void CommandInput3() //1つ前に戻るボタン
         {
             commandInput.SetInputNumber(0);
 
             startRoomPanel.SetActive(true);
-            createRoomPanel.SetActive(false);
-            joinRoomPanel.SetActive(false);
-            commonPanel.SetActive(false);
         }
     }
 }
