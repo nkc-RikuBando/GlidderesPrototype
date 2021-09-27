99e40c9ce31d85185f288632b516964ad1dce70f
GTA20matsumoto <countdata252@gmail.com>
2021-09-27 18:39:25 +0900

    BuffViewDataに演出用オブジェクトのPrefabを付与できるように

---
+++ Glidders/Assets/Scripts/GameRoom/SignalSender.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Glidders
+{
+    namespace Photon
+    {
+        public class SignalSender
+        {
+            
+        }
+    }
+}
