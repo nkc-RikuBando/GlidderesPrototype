4ec41621e25f64c4b212622175a8d8cffcb63564
nkc-TakaoOnda <tnk5zdmr7uegh4i2i13m@icloud.com>
2021-10-01 16:33:17 +0900

    なんか大規模プッシュによるバグの修正

--- Glidders/Assets/Scripts/WaitingRoom/10_Offline/Others/CharacterBoolManager.cs
+++ Glidders/Assets/Scripts/WaitingRoom/10_Offline/Others/CharacterBoolManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterBoolManager : MonoBehaviour
 {
@@ -12,8 +13,72 @@ public class CharacterBoolManager : MonoBehaviour
 
     private SpriteRenderer spriteRenderer;
 
+    [SerializeField] float rColor = 0.5f;
+    [SerializeField] float gColor = 0.5f;
+    [SerializeField] float bColor = 0.5f;
+    private int defaultColor = 1;
+
+
+    [SerializeField] Image kaitoButton;
+    [SerializeField] Image seiraButton;
+    [SerializeField] Image yuButton;
+    [SerializeField] Image mitsuhaButton;
+
     private void Start()
     {
         
+    }
+
+    private void Update()
+    {
+        NotKaitoButtoon();
+        NotSeiraButton();
+        NotYuButton();
+        NotMitsuhaButton();
+    }
+
+    public void NotKaitoButtoon()
+    {
+        if (!isSelectKaito)
+        {
+            kaitoButton.color = new Color(rColor, gColor, bColor);
+        }
+        else
+            kaitoButton.color = new Color(defaultColor,defaultColor,defaultColor);
+
+
+    }
+
+    public void NotSeiraButton()
+    {
+        if (!isSelectSeira)
+        {
+            seiraButton.color = new Color(rColor, gColor, bColor);
+        }
+        else
+            seiraButton.color = new Color(defaultColor, defaultColor, defaultColor);
+
+    }
+
+    public void NotYuButton()
+    {
+        if (!isSelectYu)
+        {
+            yuButton.color = new Color(rColor, gColor, bColor);
+        }
+        else
+            yuButton.color = new Color(defaultColor, defaultColor, defaultColor);
+
+    }
+
+    public void NotMitsuhaButton()
+    {
+        if (!isSelectMitsuha)
+        {
+            mitsuhaButton.color = new Color(rColor, gColor, bColor);
+        }
+        else
+            mitsuhaButton.color = new Color(defaultColor, defaultColor, defaultColor);
+
     }
 }
