4d1f8b85cd0746c576e4fcabc68c9b7afcf443cf
GTA20matsumoto <countdata252@gmail.com>
2021-10-02 17:00:38 +0900

    コメント作成中

--- Glidders/Assets/Editor/CommentOutputInspectorEditor.cs
+++ Glidders/Assets/Editor/CommentOutputInspectorEditor.cs
@@ -1,4 +1,4 @@
-/*using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
@@ -27,9 +27,15 @@ public class CommentOutputInspectorEditor : Editor
         //    initializeFlg = false;
         //}
         
-        EditorGUILayout.LabelField("a");
-        for (int i = 0; i < commentOutput.commentTable.Count; ++i)
+        for (int i = 0; i < commentOutput.tableSize.Count; ++i)
         {
+            commentList = new List<string>();
+            int startIndex = commentOutput.tableSize[i];
+            int endIndex;
+            if (i < commentOutput.tableSize.Count - 1)
+                endIndex = commentOutput.tableSize[i + 1];
+            else
+                endIndex = commentOutput.commentTable.Count;
             using (new EditorGUILayout.HorizontalScope())
             {
                 using (new EditorGUILayout.VerticalScope(GUI.skin.box))
@@ -43,36 +49,58 @@ public class CommentOutputInspectorEditor : Editor
                     commentOutput.foldOut[i] = EditorGUILayout.Foldout(commentOutput.foldOut[i], "“à—e");
                     if (commentOutput.foldOut[i])
                     {
-                        commentList = commentOutput.commentTable[i];
-                        for (int j = 0; j < commentOutput.commentTable[i].Count; ++j)
+                        for (int j = 0; j <commentList.Count; ++j)
                         {
                             using (new EditorGUILayout.HorizontalScope())
                             {
                                 commentList[j] = EditorGUILayout.TextField("", commentList[j]);
                                 if (GUILayout.Button("-"))
+                                {
                                     commentList.RemoveAt(i);
+                                    commentOutput.commentTable.RemoveAt(i + startIndex);
+                                }
                             }
                         }
                         if (GUILayout.Button("+"))
+                        {
                             commentList.Add("");
-                        commentOutput.commentTable[i] = commentList;
+                            commentOutput.commentTable.Insert(endIndex, "");
+                            ++endIndex;
+                            Debug.Log("endindex = " + endIndex);
+                        }
+                        for(int j = startIndex; j < endIndex; ++j)
+                        {
+                            Debug.Log("j = " + j);
+                            commentOutput.commentTable[j] = commentList[j - startIndex];
+                        }
                     }
 
                 }
                 if (GUILayout.Button("-"))
                 {
                     commentOutput.tableName.RemoveAt(i);
-                    commentOutput.commentTable.RemoveAt(i);
+                    //commentOutput.commentTable.RemoveAt(i);
                     commentOutput.commentRate.RemoveAt(i);
                     commentOutput.tableActive.RemoveAt(i);
                     commentOutput.foldOut.RemoveAt(i);
+                    for (int j = startIndex; j < endIndex; ++j)
+                    {
+                        commentOutput.commentTable.RemoveAt(j);
+                    }
+                    int removeCommentCount = endIndex - startIndex;
+                    for (int j = i + 1; j < commentOutput.tableSize.Count; ++j)
+                    {
+                        commentOutput.tableSize[j] -= removeCommentCount;
+                    }
+                    commentOutput.tableSize.RemoveAt(i);
                 }
             }
         }
         if (GUILayout.Button("+"))
         {
             commentOutput.tableName.Add("");
-            commentOutput.commentTable.Add(new List<string>());
+            //commentOutput.commentTable.Add();
+            commentOutput.tableSize.Add(commentOutput.commentTable.Count);
             commentOutput.commentRate.Add(1);
             commentOutput.tableActive.Add(true);
             commentOutput.foldOut.Add(false);
@@ -84,4 +112,4 @@ public class CommentOutputInspectorEditor : Editor
             EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
         }
     }
-}*/
+}
